36e85d5bc9243919e365e4138ce6994d2e1f854d
Jesse Nelson <J.nelson55@gmail.com>
2017-11-07 16:54:43 -0700

    New Classes

--- Project1/Project 1/Controllers/ProposalController.cs
+++ Project1/Project 1/Controllers/ProposalController.cs
@@ -6,10 +6,120 @@ using System.Web.Mvc;
 
 namespace Project_1.Controllers
 {
+    class Client
+    {
+        int clientID;
+        string ClientName;
+        string ContactName;
+        string ContactPhone;
+        string AddressLine1;
+        string AddressLine2;
+        string City;
+        string State;
+        string email;
+    }
+
     class Proposal
     {
-        //variables here
+        string ProjectName;
+        Client Client;
     }
+
+    class Proposal_details : Proposal
+    {
+        static int MinFeeOneTime;
+        static int MinFeeRegular;
+
+        //% of fee that is for engineering and for drafting, used for calculations
+        static double DraftingFeePortion;
+        static double EngineeringFeePortion;
+
+        //admin fields
+        int RequestByID;
+        DateTime DateRequested;
+        int CreatedByID;
+        DateTime DateCreated;
+        int ApproverID;
+        DateTime ApprovedOn;
+        int ProposalID;
+
+        //Client and ProjectName Inherited From parent
+
+        //Fields for calculation of fee
+        //Percent of Construction Method and Fee per Sq. Foot
+        double SqFtCost;
+        int SqFootage;
+        double EngineeringFeePercent;
+        double EngineeringFeePerSqFt;
+        double FeeSqFt;
+        double FeePercentConstruction;
+
+        //Fee based on sheets
+        int GSN;
+        int TypicalDetails;
+        int FoundationPlans;
+        int FloorFraming;
+        int RoofFraming;
+        int WallPanelElevation;
+        int SheerWallPlans;
+        int FoundationDetails;
+        int FramingDetails;
+
+        int EstHoursPerSheet;
+        int DraftingHours;
+        int EngineeringHours;
+
+        int PlanCheckComments;
+        int ShopDrawingReview;
+        int ConstructionAdmin_RFI;
+
+        int SpecialInspections;
+        double SpecialInspectionsRate;
+
+        double FeeSheets;
+
+        //Fee on Man hours
+        int GSNHours;
+        int TypicalDetailsHours;
+        int FoundationPlansHours;
+        int FloorFramingHours;
+        int RoofFramingHours;
+        int WallPanelElevationHours;
+        int SheerWallPlansHours;
+        int FoundationDetailsHours;
+        int FramingDetailsHours;
+
+        //Fee based on number of Units
+        int NumUnits;
+        double CostPerUnit;
+        int CommunityServiceArea;
+        double PricePerSqFt_CommunityServiceArea;
+        double CommunityServiceFees;
+        double RetainingWallsAndMiscFee;
+        string NoteOnMiscFees;
+        double FeeNumUnits;
+
+
+        //SummaryFields
+        double FeeAverage;
+        int NumOfFeesInCalc;
+        double EngineeringEst;
+        double DraftingEst;
+
+        double ContractAmount;
+        double EngineeringFee;
+        double DraftingFee;
+
+
+
+    }
+    class Proposal_Request : Proposal
+    {
+        int RequestByID;
+        DateTime DateRequested;
+        string RequestData;
+    }
+
     public class ProposalController : Controller
     {
         
