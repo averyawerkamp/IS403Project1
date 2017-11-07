01f2e4c8cf845917e4b370b1dde0070fcab282ae
avery <avery@DESKTOP-25UFBQ3>
2017-11-07 12:51:58 -0700

    New Controller

---
+++ Project1/Project 1/Controllers/ProposalController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_1.Controllers
+{
+    public class ProposalController : Controller
+    {
+        // GET: Proposal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewRequest()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Review()
+        {
+            return View();
+        }
+
+        public ActionResult All()
+        {
+            return View();
+        }
+
+        public ActionResult ViewProposal()
+        {
+            return View();
+        }
+    }
+}
