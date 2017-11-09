af1452c999af5268e78f35b7064b96e4ceb0ab21
BrianRaz <brianraz17@gmail.com>
2017-11-09 12:24:40 -0700

    Request class display names

--- Project1/Project 1/Models/Request.cs
+++ Project1/Project 1/Models/Request.cs
@@ -12,13 +12,19 @@ namespace Project_1.Models
     public class Request
     {
         [Key]
+        [DisplayName("Request ID")]
         public int RequestID { get; set; }
+        [DisplayName("Requester ID")]
         public int RequestedByID { get; set; }
+        [DisplayName("Request Date")]
         public DateTime RequestDate { get; set; }
         [DisplayName("Client")]
         public int ClientID { get; set; }
+        [DisplayName("Project Name")]
         public string ProjectName { get; set; }
+        [DisplayName("Description")]
         public string RequestDesc { get; set; }
+        [DisplayName("Client")]
         public virtual Client Client { get; set; }
         
 
