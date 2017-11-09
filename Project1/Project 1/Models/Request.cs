900c049b1198e42732d441202d8151942b4fe4c9
avery <avery@DESKTOP-25UFBQ3>
2017-11-08 18:23:43 -0700

    Request Page!

---
+++ Project1/Project 1/Models/Request.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Project_1.Models
+{
+    [Table("Request")]
+    public class Request
+    {
+        [Key]
+        public int RequestID { get; set; }
+        public int RequestedByID { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string ProjectName { get; set; }
+        public int ClientID { get; set; }
+        public string RequestDesc { get; set; }
+
+    }
+}
